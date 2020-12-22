081c02cfb50d572a31fc3662a4fbf7d78a91e7f0
ClaudiaEL <58369044+ClaudiaEsL@users.noreply.github.com>
2020-12-22 15:00:21 -0400

    commit

---
+++ js/seleccionar-seccion.js
@@ -0,0 +1,68 @@
+//Botones
+//Seccion 1//
+$("#boton-seccion-1").click(
+    function () {
+        $("#seccion-1").css("display","block");
+        $("#seccion-2").css("display","none");
+        $("#seccion-3").css("display","none");
+        $("#seccion-4").css("display","none");
+        $("#seccion-5").css("display","none");
+        $("#seccion-6").css("display","none");
+
+    }
+);
+//Seccion 2//
+$("#boton-seccion-2").click(
+    function () {
+        $("#seccion-1").css("display","none");
+        $("#seccion-2").css("display","block");
+        $("#seccion-3").css("display","none");
+        $("#seccion-4").css("display","none");
+        $("#seccion-5").css("display","none");
+        $("#seccion-6").css("display","none");
+    }
+);
+//Seccion 3//
+$("#boton-seccion-3").click(
+    function () {
+        $("#seccion-1").css("display","none");
+        $("#seccion-2").css("display","none");
+        $("#seccion-3").css("display","block");
+        $("#seccion-4").css("display","none");
+        $("#seccion-5").css("display","none");
+        $("#seccion-6").css("display","none");
+    }
+);
+//Seccion 4//
+$("#boton-seccion-4").click(
+    function () {
+        $("#seccion-1").css("display","none");
+        $("#seccion-2").css("display","none");
+        $("#seccion-3").css("display","none");
+        $("#seccion-4").css("display","block");
+        $("#seccion-5").css("display","none");
+        $("#seccion-6").css("display","none");
+    }
+);
+//Seccion 5//
+$("#boton-seccion-5").click(
+    function () {
+        $("#seccion-1").css("display","none");
+        $("#seccion-2").css("display","none");
+        $("#seccion-3").css("display","none");
+        $("#seccion-4").css("display","none");
+        $("#seccion-5").css("display","block");
+        $("#seccion-6").css("display","none");
+    }
+);
+//Seccion 6//
+$("#boton-seccion-6").click(
+    function () {
+        $("#seccion-1").css("display","none");
+        $("#seccion-2").css("display","none");
+        $("#seccion-3").css("display","none");
+        $("#seccion-4").css("display","none");
+        $("#seccion-5").css("display","none");
+        $("#seccion-6").css("display","block");
+    }
+);
